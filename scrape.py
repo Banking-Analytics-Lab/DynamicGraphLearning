a758814189887e2ee73c7196dd8c166742f2fe56
Elena Tiukhova <elena.tiukhova@kuleuven.be>
2022-08-01 17:49:28 +0200

    Scrape updated

--- scrape.py
+++ scrape.py
@@ -6,18 +6,24 @@ df.columns = ['State','Tag']
 clean = lambda x: x.Tag.replace('.','').lower()
 df.Tag = df.apply(clean,axis = 1)
 count = 0 
+data = ['poblacion']
+nums = [(lambda x: '0' + str(x) if x <10 else str(x))(x) for x in range(len(data))]
+
+
+
 for tag in df.Tag:
-    try: 
-        resp = requests.get(url = f'https://en.www.inegi.org.mx/contenidos/programas/ccpv/2020/tabulados/cpv2020_b_{tag}_01_poblacion.xlsx')
+    for i,dat in zip(nums,data): 
+        try: 
+            resp = requests.get(url = f'https://en.www.inegi.org.mx/contenidos/programas/ccpv/2020/tabulados/cpv2020_b_{tag}_{i}_{dat}.xlsx')
 
 
-        output = open('test.xls', 'wb')
-        output.write(resp.content)
-        output.close()
-        break
-    except Exception as e: 
-        print(f'{e=},\n{tag=}')
-        count += 1 
+            output = open('test.xls', 'wb')
+            output.write(resp.content)
+            output.close()
+            break
+        except Exception as e: 
+            print(f'{e=},\n{tag=}')
+            count += 1 
 
 
 print(count,len(df))
