5363da06497362abe0d40de09d96b66d4a72f87d
Elena Tiukhova <elena.tiukhova@kuleuven.be>
2022-08-02 11:07:17 +0200

    Scrapped files and socio-economic analysis script are added

--- scrape.py
+++ scrape.py
@@ -1,26 +1,30 @@
+
 import requests
 import pandas as pd 
 
-df = pd.read_csv('/home/emiliano/projects/def-cbravo/emiliano/RappiAnomalyDetection/dict.csv',header = None )
+df = pd.read_csv('dict.csv',header = None )
 df.columns = ['State','Tag']
 clean = lambda x: x.Tag.replace('.','').lower()
 df.Tag = df.apply(clean,axis = 1)
 count = 0 
-data = ['poblacion']
-nums = [(lambda x: '0' + str(x) if x <10 else str(x))(x) for x in range(len(data))]
+data = ['poblacion', 'fecundidad', 'mortalidad', 'migracion', 'caracteristicas_economicas']
+nums = ['01', '02', '03','04','08']
 
 
 
 for tag in df.Tag:
+    print(tag)
     for i,dat in zip(nums,data): 
+        
         try: 
             resp = requests.get(url = f'https://en.www.inegi.org.mx/contenidos/programas/ccpv/2020/tabulados/cpv2020_b_{tag}_{i}_{dat}.xlsx')
-
-
-            output = open('test.xls', 'wb')
+            print(i, dat)
+            
+            output = open(f'./scrapped/{tag}{i}{dat}.xlsx', 'wb')
+            
             output.write(resp.content)
             output.close()
-            break
+            
         except Exception as e: 
             print(f'{e=},\n{tag=}')
             count += 1 
